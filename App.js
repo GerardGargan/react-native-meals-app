6efae08d1ebdfcd3a103cea860fce73cb3b52db4
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2024-11-01 23:22:21 +0000

    setup meals page and navigation

--- App.js
+++ App.js
@@ -3,7 +3,9 @@ import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
 
+
 import CategoriesScreen from './screens/CategoriesScreen';
+import MealsOverviewScreen from './screens/MealsOverviewScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,8 +14,9 @@ export default function App() {
     <>
       <StatusBar style='dark' />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator initialRouteName='MealsCategories'>
           <Stack.Screen name='MealsCategories' component={CategoriesScreen} />
+          <Stack.Screen name='MealsOverview' component={MealsOverviewScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
