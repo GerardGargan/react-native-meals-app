80a33a59fd1e33b67274d1c8965f731e09ecf658
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2024-11-02 20:47:58 +0000

    display meal items

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,12 +1,28 @@
+import MealItem from "../components/MealItem";
 import { MEALS } from "../data/dummy-data";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 export default function MealsOverviewScreen({ route }) {
 
     const { categoryId } = route.params;
 
+    const displayedMeals = MEALS.filter((meal) => {
+        return meal.categoryIds.indexOf(categoryId) >= 0;
+    });
+
+    function renderMealItem(itemData) {
+        return (
+            <MealItem title={itemData.item.title} />
+        );
+    }
+
     return (
         <View style={styles.container}>
             <Text>Meals Overview Screen - {categoryId}</Text>
+            <FlatList 
+            data={displayedMeals}
+            renderItem={renderMealItem}
+            keyExtractor={(item) => item.id}
+            />
         </View>
     );
 }
