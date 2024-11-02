c1fafe63c360a30f75203a0f21fd6f76394f4b16
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2024-11-02 21:12:29 +0000

    add styling and detail to meal item

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -2,34 +2,39 @@ import MealItem from "../components/MealItem";
 import { MEALS } from "../data/dummy-data";
 import { View, Text, StyleSheet, FlatList } from "react-native";
 export default function MealsOverviewScreen({ route }) {
+  const { categoryId } = route.params;
 
-    const { categoryId } = route.params;
+  const displayedMeals = MEALS.filter((meal) => {
+    return meal.categoryIds.indexOf(categoryId) >= 0;
+  });
 
-    const displayedMeals = MEALS.filter((meal) => {
-        return meal.categoryIds.indexOf(categoryId) >= 0;
-    });
-
-    function renderMealItem(itemData) {
-        return (
-            <MealItem title={itemData.item.title} />
-        );
-    }
-
+  function renderMealItem(itemData) {
     return (
-        <View style={styles.container}>
-            <Text>Meals Overview Screen - {categoryId}</Text>
-            <FlatList 
-            data={displayedMeals}
-            renderItem={renderMealItem}
-            keyExtractor={(item) => item.id}
-            />
-        </View>
+      <MealItem 
+      title={itemData.item.title} 
+      imageUrl={itemData.item.imageUrl} 
+      duration={itemData.item.duration}
+      complexity={itemData.item.complexity}
+      affordability={itemData.item.affordability}
+      />
     );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Meals Overview Screen - {categoryId}</Text>
+      <FlatList
+        data={displayedMeals}
+        renderItem={renderMealItem}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 16,
-    }
+  container: {
+    flex: 1,
+    padding: 16,
+  },
 });
